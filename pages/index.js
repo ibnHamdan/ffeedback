a8dee3a619fd62fa7edc95975ca115f23fa8e0f1
ibnHamdan <ibnHamdan@msn.com>
2021-08-23 23:32:27 +0300

    add firebase

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { auth } from "firebase";
+import { useAuth } from "../lib/auth";
 
 export default function Home() {
+  const auth = useAuth();
   return (
     <div className={styles.container}>
       <Head>
@@ -14,31 +17,11 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>F-Feedback</h1>
 
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.js</code>
-        </p>
+        <button onClick={(e) => auth.singinWithGithub()}>Sign In With GitHub</button>
+        <button onClick={(e) => auth.signinWithGoogle()}>Sign In With Google</button>
+        {auth?.user && <button onClick={(e) => auth.signout()}>Sign Out</button>}
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" className={styles.card}>
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
+        <div>{auth?.user?.email}</div>
       </main>
 
       <footer className={styles.footer}>
@@ -46,12 +29,7 @@ export default function Home() {
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        ></a>
       </footer>
     </div>
   );
